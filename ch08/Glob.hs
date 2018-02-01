fe2322ef65b0c021420484f3d53414bc3c52dd39
Dillon Peng <pengcz.nwpu@gmail.com>
2018-02-01 13:08:58 +0800

    ch08

---
+++ ch08/Glob.hs
@@ -0,0 +1,13 @@
+module Glob
+  (
+    namesMatching
+  )where
+
+import System.Directory (doesDirectoryExist, doesFileExist,
+                         getCurrentDirectory, getDirectoryContents)
+
+import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>))
+
+import Control.Exception (handle)
+import Control.Monad (forM)
+import GlobRegex (matchesGlob)
